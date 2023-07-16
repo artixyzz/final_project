c4fc3df27785a90e0f766428baa420020944fcd1
Artur Kubiak <artixyz1998@gmail.com>
2023-07-16 11:40:44 +0200

    DataBase

--- src/main/java/com/example/final_project/model/Course.java
+++ src/main/java/com/example/final_project/model/Course.java
@@ -28,4 +28,5 @@ public class Course {
 
     private LocalTime courseDuration;
 
+
 }
--- src/main/java/com/example/final_project/reposiroty/UserRepository.java
+++ src/main/java/com/example/final_project/reposiroty/UserRepository.java
@@ -4,4 +4,6 @@ import com.example.final_project.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User,Long> {
+    User findByEmailOrLogin(String email, String login);
+
 }
