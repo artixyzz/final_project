886f9983341cabc77be16a8f2f9f194db797e900
Artur Kubiak <artixyz1998@gmail.com>
2023-07-22 15:57:02 +0200

    Changes2

--- src/main/java/com/example/final_project/controller/HomeController.java
+++ src/main/java/com/example/final_project/controller/HomeController.java
@@ -1,19 +1,38 @@
 package com.example.final_project.controller;
 
+import com.example.final_project.model.Course;
+import com.example.final_project.service.CourseService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 @Controller
 public class HomeController {
+
+    private final CourseService courseService;
+
+    public HomeController(CourseService courseService) {
+        this.courseService = courseService;
+    }
 
     @GetMapping("/")
     public String homePage (){
         return "homepage";
     }
 
-    @GetMapping("/Wszystkie kursy")
-    public String allCourses() {
-        return "allCourses";
-    }
+
+//    @GetMapping("/courses")
+//        public ResponseEntity<List<Course>>findAllCourses(@RequestParam(required = false) String courseName) {
+//
+//        List<Course> courses = new ArrayList<Course>();
+//
+//        return "courses";
+//
+//    }
 
 }
---
+++ src/main/java/com/example/final_project/service/SubjectService.java
@@ -0,0 +1,7 @@
+package com.example.final_project.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SubjectService {
+}
