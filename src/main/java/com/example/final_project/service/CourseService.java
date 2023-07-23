7cc04c4a09f3a6076898bdf8353db95b7e5c7ff1
Artur Kubiak <artixyz1998@gmail.com>
2023-07-23 10:27:53 +0200

    Changes

--- src/main/java/com/example/final_project/service/CourseService.java
+++ src/main/java/com/example/final_project/service/CourseService.java
@@ -1,42 +1,40 @@
 package com.example.final_project.service;
-
 import com.example.final_project.model.Course;
+import com.example.final_project.reposiroty.CourseRepository;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Service;
-
 import java.util.ArrayList;
 import java.util.List;
-
+@AllArgsConstructor
 @Service
 public class CourseService {
-
-//    Dodać metodę assignCourse
-    static List<Course> courses = new ArrayList<Course>();
-
+    private final CourseRepository courseRepository;
+    //    Dodać metodę assignCourse
     public List<Course> findAllCourses() {
-        return courses;
+        return courseRepository.findAll();
     }
-
     public Course findById(long id) {
-        return courses.stream().filter(course-> id == course.getId()).findAny().orElse(null);
+        return courseRepository.findById(id).orElseThrow(() -> new RuntimeException("Not found"));
     }
 
-    public Course saveCourse(Course course) {
-        if (course.getId() != 0) {
-            long id = course.getId();
-
-            for (int i = 0; i < courses.size(); i++)
-                if (id == courses.get(i).getId()) {
-                    courses.set(i, course);
-                    break;
-                }
-
-            return course;
-        }
-        courses.add(course);
-        return course;
-    }
-       public void deleteCourseById (long id){
-           courses.removeIf(course -> id == course.getId());
-        }
-
+//    public Course saveCourse(Course course) {
+//        if (course.getId() != 0) {
+//            long id = course.getId();
+//
+//            for (int i = 0; i < courses.size(); i++)
+//                if (id == courses.get(i).getId()) {
+//                    courses.set(i, course);
+//                    break;
+//                }
+//
+//            return course;
+//        }
+//        courses.add(course);
+//        return course;
+//    }
+//
+//    public void deleteCourseById(long id) {
+//        courses.removeIf(course -> id == course.getId());
+//    }
 }
