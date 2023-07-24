22605cbd84655b1dd0b5e4d0e8738e998cfba3ae
CaniJavi <michalmachnio9@gmail.com>
2023-07-24 21:09:56 +0200

    Dodanie podstron do subjectów

--- src/main/java/com/example/final_project/controller/HomeController.java
+++ src/main/java/com/example/final_project/controller/HomeController.java
@@ -1,4 +1,5 @@
 package com.example.final_project.controller;
+
 import com.example.final_project.dto.RequestCourseDto;
 import com.example.final_project.service.CourseService;
 import com.example.final_project.service.SubjectService;
@@ -15,27 +16,78 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class HomeController {
     private final CourseService courseService;
     private final SubjectService subjectService;
+
     @GetMapping("/")
     public String homePage() {
         return "homePage";
     }
+
     @GetMapping("/courses")
     public String findAllCourses(Model model) {
         model.addAttribute("courses", courseService.findAllCourses());
         return "courses";
     }
+
     @GetMapping("/courses/create")
-    public String createCourse(Model model){
+    public String createCourse(Model model) {
         model.addAttribute("subject", subjectService.findAllSubjects());
         model.addAttribute("course", RequestCourseDto.builder().build());
         return "courseCreate";
     }
+
     @PostMapping("/courses/create")
-    public String saveCourse(Model model, @Valid @ModelAttribute RequestCourseDto requestCourseDto){
+    public String saveCourse(Model model, @Valid @ModelAttribute RequestCourseDto requestCourseDto) {
         courseService.save(requestCourseDto);
-        model.addAttribute("courses", courseService.findAllCourses());
+        model.addAttribute("courses",  courseService.findAllCourses());
         return "/courses";
+
+    }
+
+    @GetMapping("/subjects/1")
+    public String subjects1(Model model) {
+        model.addAttribute("subject", subjectService.findAllSubjects());
+        return "MySQL";
+    }
+
+    @GetMapping("/subjects/2")
+    public String subjects2(Model model) {
+        model.addAttribute("subject", subjectService.findAllSubjects());
+        return "JavaBasic";
+
+
+    }
+
+    @GetMapping("/subjects/3")
+    public String subjects3(Model model) {
+        model.addAttribute("subject", subjectService.findAllSubjects());
+        return "JavaAdvanced";
+
+    }
+
+    @GetMapping("/subjects/4")
+    public String subjects4(Model model) {
+        model.addAttribute("subject", subjectService.findAllSubjects());
+        return "PythonBasic";
+
+    }
+
+    @GetMapping("/subjects/5")
+    public String subjects5(Model model) {
+        model.addAttribute("subject", subjectService.findAllSubjects());
+        return "PythonAdvanced";
+
+    }
+
+    @GetMapping("/subjects/6")
+    public String subjects6(Model model) {
+        model.addAttribute("subject", subjectService.findAllSubjects());
+        return "C++Basic";
+
+    }
+
+    @GetMapping("/subjects/7")
+    public String subjects7(Model model) {
+        model.addAttribute("subject", subjectService.findAllSubjects());
+        return "C++Advanced";
     }
 }
-
-
--- src/main/java/com/example/final_project/model/Course.java
+++ src/main/java/com/example/final_project/model/Course.java
@@ -15,6 +15,7 @@ import java.util.List;
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
+
 public class Course {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
