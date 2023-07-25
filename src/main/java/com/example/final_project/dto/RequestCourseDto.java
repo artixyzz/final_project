e689950856512bc9e419273098ac217ef0646a4d
CaniJavi <michalmachnio9@gmail.com>
2023-07-25 20:02:46 +0200

    Dodanie podstron do subjectów

--- src/main/java/com/example/final_project/dto/RequestCourseDto.java
+++ src/main/java/com/example/final_project/dto/RequestCourseDto.java
@@ -5,8 +5,6 @@ import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Builder;
 import lombok.Data;
@@ -14,9 +12,7 @@ import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.Range;
 
 import java.time.LocalDate;
-import java.time.LocalTime;
 import java.util.List;
-
 @Data
 @Builder
 public class RequestCourseDto {
