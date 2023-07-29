d8549115b8dfa982769d5e44dcd0b3e09532a1ca
KlaudiaSto <119117291+KlaudiaSto@users.noreply.github.com>
2023-07-29 13:24:06 +0200

    Redukcja HTML

--- src/main/java/com/example/final_project/reposiroty/LessonRepository.java
+++ src/main/java/com/example/final_project/reposiroty/LessonRepository.java
@@ -3,6 +3,8 @@ package com.example.final_project.reposiroty;
 import com.example.final_project.model.Lesson;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface LessonRepository extends JpaRepository<Lesson,Long> {
 
 }
