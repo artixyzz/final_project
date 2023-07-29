4f3998cd933bf469bf0f9725b8a37dd536a56160
Artur Kubiak <artixyz1998@gmail.com>
2023-07-29 13:39:52 +0200

    Changes

--- src/main/java/com/example/final_project/controller/HomeController.java
+++ src/main/java/com/example/final_project/controller/HomeController.java
@@ -2,21 +2,19 @@ package com.example.final_project.controller;
 import com.example.final_project.dto.CourseDto;
 import com.example.final_project.dto.RequestCourseDto;
 import com.example.final_project.service.CourseService;
+import com.example.final_project.service.LessonService;
 import com.example.final_project.service.SubjectService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import org.springframework.web.servlet.view.RedirectView;
-import org.xml.sax.Attributes;
-
 @AllArgsConstructor
 @Controller
 public class HomeController {
     private final CourseService courseService;
     private final SubjectService subjectService;
+    private final LessonService lessonService;
     @GetMapping("/")
     public String homePage() {
         return "homePage";
@@ -34,11 +32,11 @@ public class HomeController {
         model.addAttribute("courses", courseService.findAllCourses());
         return "courses";
     }
-    @GetMapping("/subjects")
-    public String findAllSubjects(Model model) {
-        model.addAttribute("subjects", subjectService.findAllSubjects());
-        return "subjects/1";
-    }
+    //    @GetMapping("/subjects")
+//    public String findAllSubjects(Model model) {
+//        model.addAttribute("subjects", subjectService.findAllSubjects());
+//        return "subjects/1";
+//    }
     @GetMapping("/courses/create")
     public String createCourse(Model model) {
         model.addAttribute("subject", subjectService.findAllSubjects());
@@ -71,39 +69,16 @@ public class HomeController {
 //            return new RedirectView("courses");
         return "redirect:/courses";
     }
-    @GetMapping("/subjects/1")
-    public String subjects1(Model model) {
-        model.addAttribute("subject", subjectService.findAllSubjects());
-        return "MySQL";
-    }
-    @GetMapping("/subjects/2")
-    public String subjects2(Model model) {
-        model.addAttribute("subject", subjectService.findAllSubjects());
-        return "JavaBasic";
-    }
-    @GetMapping("/subjects/3")
-    public String subjects3(Model model) {
-        model.addAttribute("subject", subjectService.findAllSubjects());
-        return "JavaAdvanced";
-    }
-    @GetMapping("/subjects/4")
-    public String subjects4(Model model) {
-        model.addAttribute("subject", subjectService.findAllSubjects());
-        return "PythonBasic";
-    }
-    @GetMapping("/subjects/5")
-    public String subjects5(Model model) {
-        model.addAttribute("subject", subjectService.findAllSubjects());
-        return "PythonAdvanced";
-    }
-    @GetMapping("/subjects/6")
-    public String subjects6(Model model) {
-        model.addAttribute("subject", subjectService.findAllSubjects());
-        return "C++Basic";
-    }
-    @GetMapping("/subjects/7")
-    public String subjects7(Model model) {
-        model.addAttribute("subject", subjectService.findAllSubjects());
-        return "C++Advanced";
+    @GetMapping("/subjects/{id}")
+    public String subjects(Model model,@PathVariable Long id) {
+        model.addAttribute("subject", subjectService.findById(id));
+        return "subject";
     }
 }
+
+
+
+
+
+
+
